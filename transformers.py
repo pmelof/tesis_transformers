2138fbbe270e089df6e03b6ecbd5e45a5b4a3341
Patricia Melo <patricia.melo@usach.cl>
2023-06-02 00:34:16 -0400

    primera vez corre transformers

--- transformers.py
+++ transformers.py
@@ -2,6 +2,7 @@
 
 import math
 import numpy as np
+import h5py
 import os
 from tempfile import TemporaryDirectory
 from typing import Tuple
@@ -10,7 +11,7 @@ import torch
 from torch import nn, Tensor
 import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
-from torch.utils.data import dataset
+from torch.utils.data import dataset, DataLoader
 # tiempo
 import time
 
@@ -26,7 +27,8 @@ class TransformerModel(nn.Module):
         self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
         self.encoder = nn.Embedding(ntoken, d_model)
         self.d_model = d_model
-        self.decoder = nn.Linear(d_model, ntoken)
+        # self.decoder = nn.Linear(d_model, ntoken)
+        self.decoder = nn.Linear(d_model*116, 2) # salida de Y: velocidad (v_x, v_y)
 
         self.init_weights()
 
@@ -48,6 +50,7 @@ class TransformerModel(nn.Module):
         src = self.encoder(src) * math.sqrt(self.d_model)
         src = self.pos_encoder(src)
         output = self.transformer_encoder(src, src_mask)
+        output = output.view(src.shape[0], -1)
         output = self.decoder(output)
         return output
 
@@ -105,6 +108,53 @@ ya estan de la forma que requiero.
 Lo que si debo hacer es separar el dataset en train, validate y test
 '''
 
+def dataset_preprocessing(filepath_dataset: str , filename_dataset: str , rounded_decimal: int = 1 ):
+    """
+    Lee el archivo dataset baks y redondea los elementos al decimal indicado como parámetro.
+    Este preprocesamiento es para SUA y MUA.
+    Guarda los datos preprocesados en un archivo .h5 con: X_sua, X_mua e y_task.
+    ------------
+    Parámetros: 
+    filepath_dataset: String
+        Dirección donde se enceuntra el dataset baks.
+    filename_dataset: String
+        Nombre del archivo dataset baks.        
+    rounded_decimal: Int
+        Decimal al que se quiere redondear los datos, por defecto 1.
+    -------------
+    """
+    with h5py.File(filepath_dataset, 'r') as f:
+        X_sua = f[f'X_sua'][()]
+        X_mua = f[f'X_mua'][()]
+        y_task = f['y_task'][()]   
+        
+    assert len(X_sua) == len(X_mua) and len(X_sua) == len(y_task),\
+            "Largo de X_sua, X_mua e y_task no coinciden"
+    
+    # Para SUA
+    i=0
+    while i < len(X_sua):
+        j=0
+        while j < len(X_sua[0]):
+            X_sua[i][j] = round(X_sua[i][j], rounded_decimal)
+            j = j + 1
+        i = i + 1
+        
+    # Para MUA
+    i=0
+    while i < len(X_mua):
+        j=0
+        while j < len(X_mua[0]):
+            X_mua[i][j] = round(X_mua[i][j], rounded_decimal)
+            j = j + 1
+        i = i + 1
+        
+    # guardar en un archivo
+    with h5py.File(f"datos/05_rounded/{filename_dataset[:-3]}_rounded_{rounded_decimal}.h5", 'w') as f:      
+        f['X_sua'] = X_sua
+        f['X_mua'] = X_mua
+        f['y_task'] = y_task
+
 
 def generate_vocabulary(data, decimal):
     '''
@@ -139,7 +189,56 @@ def generate_vocabulary(data, decimal):
     fin = time.time()
     print(f"vocabulary time:{fin-ini}")
     return vocabulary, maxi
-            
+
+
+class Dataset_transformers(torch.utils.data.Dataset):
+    def __init__(self, X, Y, decimal):
+        """
+        Lee el archivo dataset para trabajar con DL.
+        ------------
+        Parámetros: 
+        filepath_dataset: String
+            Dirección donde se enceuntra el dataset.
+        feature: np array
+            SUA o MUA.
+        velocity: Boolean
+            Si quiere solo la velocidad para la salida.
+        -------------
+        Retorna:
+        X: np array
+            Dataset SUA o MUA, tasa de spikes estimada.
+        y: np array
+            Si velocity=True, entonces y solo contiene velocidad x e y del mono, si velocity=False, entonces y tiene posición, velocidad y aceleración x e y del mono.
+        """
+        self.decimal = decimal
+        self.X = np.multiply(X, 10**self.decimal).astype(int)
+        self.Y = Y      
+
+        assert len(self.X) == len(self.Y)
+
+    def __getitem__(self, idx):           
+        return self.X[idx], self.Y[idx]
+    
+    def __len__(self):
+        return len(self.X)
+    
+
+
+def split_dataset(filepath_dataset: str, feature: str, decimal: int, velocity: bool = True):
+    with h5py.File(filepath_dataset, 'r') as f:
+        X = f[f'X_{feature}'][()]
+        Y = f['y_task'][()]   
+    if velocity:
+        # select the x-y velocity components
+        Y = Y[:,2:4] # data shape: n x 6 (x-y position, x-y velocity, x-y acceleration)
+    
+    limit_train = int(len(X)*.8)
+    limit_eval = int(len(X)*.9)
+    ds_train = Dataset_transformers(X[:limit_train], Y[:limit_train], decimal=decimal)
+    ds_eval = Dataset_transformers(X[limit_train:limit_eval], Y[limit_train:limit_eval], decimal=decimal)
+    ds_test = Dataset_transformers(X[limit_eval:], Y[limit_eval:], decimal=decimal)   
+    
+    return ds_train, ds_eval, ds_test, X
     
 ######################################################################
 # Functions to generate input and target sequence
@@ -157,8 +256,158 @@ Esto  lo realizare con la clase DataLoader, el batch por el momento de forma arb
 # Initiate an instance
 # --------------------
 #
+# lectura datos
+# 'indy_20161005_06_baks.h5' es archivo más pequeño 4,8     
+filename_dataset = 'indy_20161005_06_baks.h5'
+# tokenización
+ini = time.time()
+decimal=1
+dataset_preprocessing(filepath_dataset=f"datos/03_baks/{filename_dataset}", filename_dataset=filename_dataset, rounded_decimal=decimal)
+fin = time.time()
+print(f"tokenization time:{fin-ini}")
+
+batch_size = 32
+
+train_ds, eval_ds, test_ds, X = split_dataset(f"datos/05_rounded/{filename_dataset[:-3]}_rounded_{decimal}.h5", "sua", decimal, velocity=True)
+train_dl = DataLoader(train_ds, batch_size, shuffle=False)
+eval_dl = DataLoader(eval_ds, batch_size, shuffle=False)
+test_dl = DataLoader(test_ds, batch_size, shuffle=False)
+# Genero vocabulario, ahora por un solo archivo 
+# después busco el máximo y genero un array que abarque a todos los archivos.
+print(f"Generando vocabulario ... ")
+vocabulary, maxi = generate_vocabulary(X, decimal=decimal)
+print(len(vocabulary), maxi)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
 ######################################################################
 # The model hyperparameters are defined below. The ``vocab`` size is
 # equal to the length of the vocabulary.
 #
+ntokens = len(vocabulary)  # size of vocabulary
+emsize = 200  # embedding dimension
+d_hid = 200  # dimension of the feedforward network model in ``nn.TransformerEncoder``
+nlayers = 2  # number of ``nn.TransformerEncoderLayer`` in ``nn.TransformerEncoder``
+nhead = 2  # number of heads in ``nn.MultiheadAttention``
+dropout = 0.2  # dropout probability
+model = TransformerModel(ntokens, emsize, nhead, d_hid, nlayers, dropout).to(device)
 
+
+######################################################################
+# Run the model
+# -------------
+#
+
+
+######################################################################
+# We use `CrossEntropyLoss <https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html>`__
+# with the `SGD <https://pytorch.org/docs/stable/generated/torch.optim.SGD.html>`__
+# (stochastic gradient descent) optimizer. The learning rate is initially set to
+# 5.0 and follows a `StepLR <https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.StepLR.html>`__
+# schedule. During training, we use `nn.utils.clip_grad_norm\_ <https://pytorch.org/docs/stable/generated/torch.nn.utils.clip_grad_norm_.html>`__
+# to prevent gradients from exploding.
+#
+
+import time
+
+criterion = nn.CrossEntropyLoss()
+# criterion = nn.MSELoss()
+lr = 5.0  # learning rate
+optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+def train(model: nn.Module, epoch: int) -> None:
+    model.train()  # turn on train mode
+    total_loss = 0.
+    log_interval = 10
+    start_time = time.time()
+    src_mask = generate_square_subsequent_mask(batch_size).to(device)
+    
+
+    num_batches = len(train_ds) // batch_size
+    # for batch, i in enumerate(range(0, train_data.size(0) - 1, batch_size)):
+        # data, targets = get_batch(train_data, i)
+    for batch, (data, targets) in enumerate(train_dl):
+        seq_len = data.size(0)
+        if seq_len != batch_size:  # only on last batch
+            src_mask = src_mask[:seq_len, :seq_len]
+        output = model(data, src_mask)
+        # loss = criterion(output.view(-1, ntokens), targets)
+        loss = criterion(output, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+        if batch % log_interval == 0 and batch > 0:
+            lr = scheduler.get_last_lr()[0]
+            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+            cur_loss = total_loss / log_interval
+            #ppl = math.exp(cur_loss)
+            print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+                  f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+                  f'loss {cur_loss:5.2f} ')
+            total_loss = 0
+            start_time = time.time()
+
+def evaluate(model: nn.Module, eval_data) -> float:
+    model.eval()  # turn on evaluation mode
+    total_loss = 0.
+    src_mask = generate_square_subsequent_mask(batch_size).to(device)
+    with torch.no_grad():
+        # for i in range(0, eval_data.size(0) - 1, batch_size):
+        #     data, targets = get_batch(eval_data, i)
+        for i, (data, targets) in enumerate(eval_data):
+            seq_len = data.size(0)
+            if seq_len != batch_size:
+                src_mask = src_mask[:seq_len, :seq_len]
+            output = model(data, src_mask)
+            # output_flat = output.view(-1, ntokens)
+            output_flat = output
+            total_loss += seq_len * criterion(output_flat, targets).item()
+    return total_loss / (len(eval_data) - 1)
+
+######################################################################
+# Loop over epochs. Save the model if the validation loss is the best
+# we've seen so far. Adjust the learning rate after each epoch.
+
+best_val_loss = float('inf')
+epochs = 3
+
+with TemporaryDirectory() as tempdir:
+    best_model_params_path = os.path.join(tempdir, "best_model_params.pt")
+
+    for epoch in range(1, epochs + 1):
+        epoch_start_time = time.time()
+        train(model, epoch=epoch)
+        val_loss = evaluate(model, eval_dl)
+        # val_ppl = math.exp(val_loss)
+        elapsed = time.time() - epoch_start_time
+        print('-' * 89)
+        print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+            f'valid loss {val_loss:5.2f}')
+        print('-' * 89)
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save(model.state_dict(), best_model_params_path)
+
+        scheduler.step()
+    model.load_state_dict(torch.load(best_model_params_path)) # load best model states
+
+
+######################################################################
+# Evaluate the best model on the test dataset
+# -------------------------------------------
+#
+
+test_loss = evaluate(model, test_dl)
+#test_ppl = math.exp(test_loss)
+print('=' * 89)
+print(f'| End of training | test loss {test_loss:5.2f}')
+print('=' * 89)
+
+print("HOla")
